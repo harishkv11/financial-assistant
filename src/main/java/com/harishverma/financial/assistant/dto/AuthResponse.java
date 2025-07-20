a1e61ddcb2808d3c9a16e7e5a3177b7c7f981f7e
harishkv11 <harish.kverma1199@gmail.com>
2025-07-20 10:46:18 +0530

    Fixed lombok issue

--- src/main/java/com/harishverma/financial/assistant/dto/AuthResponse.java
+++ src/main/java/com/harishverma/financial/assistant/dto/AuthResponse.java
@@ -9,14 +9,6 @@ import lombok.*;
 public class AuthResponse {
     private String token;
 
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
     public AuthResponse(String token) {
         this.token = token;
     }
--- src/main/java/com/harishverma/financial/assistant/security/JwtUtil.java
+++ src/main/java/com/harishverma/financial/assistant/security/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 5 Sec
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
